1fdd79048d71a1da71a778a322af7e6f78136b85
GiulianoDami <giuliano.dami@qbrobotics.com>
2024-10-03 10:47:17 +0200

    Added Interpreter Commands

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
-**Overview**
+## Overview
 
-*control_loop.py* runs the main script, it uses a RTDE socket connection to send waypoints to the UR. It also subscribes to a mqtt topic. 
-The project aim to create an interface between Vision (who pubs wp through mqtt) and UR (who listen to socket).
+This repo contains 2 ways to remotely control an Universal Robot E-series with low computational effort and just a TCP/IP connection:
+- RTDE waypoint
+- Interpreter Commands 
 
 **Prerequisites**
 - Python version at least 3.10.
 - Paho lib for mqtt, you can install with ```pip3 install paho-mqtt```.
 - An mqtt broker: for example mosquitto (```sudo apt install mosquitto```).
+
+## RTDE waypoint 
+
+With this method the task runs on an *.urp* script on Polyscope, while the PC just gives the target waypoint and some flags to syncronize. On PC side *control_loop.py* runs the main script, it uses a RTDE socket connection to send data to the UR. It also subscribes to a mqtt topic. 
+The project aim to create an interface between Vision (who pubs wp through mqtt) and UR (who listen to socket).
 
 **Usage**
 
@@ -19,3 +25,21 @@ To test this project:
 5) To fake vision commands, run ```python publisher.py```. It will send the pick targets read from *targets.txt*.
 
 The integration of the vision system, other than calibration, needs to have a part of code that sends targets through mqtt as *publisher.py* does.
+
+## Interpreter Commands 
+
+With this method the PC sends the commands to Polyscope, while on the UR runs an *.urp* script that makes it ready to receive commands. On PC side *commanding_Interp.py* runs the main script, it reads the commands of the task in a txt file and it can execute trajectories of several points (at the moment it just reads them from another txt file). It also subscribes to a mqtt topic. 
+This way could be more suited for integration scenarios where the tool has to follow some external calculated trajectory...
+
+**Usage**
+
+To test this project:
+
+1) Powerup the UR (you should have the **interpreter.urp** script inside */programs/RemoteOperation/* dir).
+2) TCP/IP connect PC to UR.
+3) In script directory, run ```python commanding_Interp.py```.
+4) Enter commands from keyboards (or read them from commands.txt, depends on the code...)
+5) To fake vision commands, run ```python publisher.py```. It will send the pick targets read from *targets.txt*.
+
+The integration of the vision system, other than calibration, needs to have a part of code that sends targets through mqtt as *publisher.py* does.
+
---
+++ commanding_interpreter/sendCommandsInterpreter.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+
+import logging
+import time
+import sys
+sys.path.append('..')
+
+from interpreter.interpreter import InterpreterHelper
+
+import mqtt.subscriber as mqtt_sub
+
+# num of commands after which clear_interpreter() command will be invoked.
+# If interpreted statements are not cleared periodically then "runtime too much behind" error may
+# be shown when leaving interpreter mode
+CLEARBUFFER_LIMIT = 500
+
+# logging.basicConfig(level=logging.INFO)
+
+
+def send_cmd_interpreter_mode_mqtt(intrp,commandFile,sub):
+    f = open(commandFile, "r")
+    trajectory_points = f.readlines()
+    command_count = 1
+    points_index = 0
+    executing_traj = False
+
+    while True:
+
+        if(not executing_traj):
+            line = input("Enter Command to execute: ")
+            match line:
+                case "EXE":
+                    executing_traj = True
+                    point_index = 0
+                    continue
+
+                case "CLOSE":
+                    line = "qbdevice.setClawCommand(2000,0)"
+
+                case "OPEN":
+                    line = "qbdevice.setClawCommand(-500,0)"
+
+                case "WAIT TARGET":
+                    while not sub.received_msg:
+                        time.sleep(0.1)
+                    line = "movel(p" + sub.stored_position + ", a=0.5, v=0.25, r=0.05)"
+
+                case _:
+                    break
+
+        else:
+            line = "movel(" + trajectory_points[point_index].rstrip() + ", a=0.5, v=0.25, r=0.05)"
+            point_index = point_index + 1
+            if point_index == len(trajectory_points):
+                executing_traj = False
+                point_index = 0
+
+        command_id = intrp.execute_command(line)
+        if command_count % CLEARBUFFER_LIMIT == 0:
+            # logging.info(f"{command_count} commands sent. Waiting for all commands to be executed before clear.")
+            # Wait for interpreted commands to be executed. New commands will be discarded if interpreter buffer
+            # limit is exceeded.
+            while intrp.get_last_executed_id() != command_id:
+                # logging.info(f"Last executed id {intrp.get_last_executed_id()}/{command_id}")
+                time.sleep(2)
+
+            # Manual buffer clear is necessary when large amount of statements is sent in one interpreter mode session.
+            # By default statements are cleared when leaving interpreter mode.
+            # Look at CLEARBUFFER_LIMIT comment for more info.
+            # logging.info("Clearing all interpreted statements")
+            intrp.clear()
+        command_count += 1
